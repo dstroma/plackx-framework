03de310d9a30f2f976866b67d456c24498a002bf
Dondi Michael Stroma <dstroma@gmail.com>
2023-07-23 02:24:18 -0400

    Minor changes and documentation

--- lib/PlackX/Framework.pm
+++ lib/PlackX/Framework.pm
@@ -14,31 +14,34 @@ use Module::Loaded ();
 #use PlackX::Framework::Template ();
 #use PlackX::Framework::URI (); 
 
-our %modules = (
-  'App'              => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
-  'Request'          => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
-  'Response'         => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
-  'Router'           => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
-  'Router::Engine'   => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
-  'Template'         => { auto_load_subclass => 1, auto_create_subclass => 1 },
-  'URI'              => { auto_load_subclass => 1 },
-);
+sub modules {
+  return (
+    'App'              => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
+    'Request'          => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
+    'Response'         => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
+    'Router'           => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
+    'Router::Engine'   => { required => 1, auto_load_subclass => 1, auto_create_subclass => 1 },
+    'Template'         => { auto_load_subclass => 1, auto_create_subclass => 1 },
+    'URI'              => { auto_load_subclass => 1 },
+  );
+}
 
 sub import {
-  # "use"ing this module will load or create subclasses in caller's namespace
-  my $caller = caller(0);
+  my $caller = caller(0);  # use this module will load or create subclasses in caller's namespace
 
   # Load the application's subclassed versions of PlackX::Framework::*
+  my %modules = modules();
   foreach my $module (keys %modules) {
     my %loaded = ();
 
+    # Attempt to load the user's subclass of the respective module
     if ($modules{$module}->{auto_load_subclass}) {
       $loaded{$module} = load_subclass($caller, $module);
     }
 
-    # Automatically create any that don't exist, unless auto_create_subclass is false
-    if ($modules{$module}->{auto_create_subclass}) {
-      generate_subclass($caller . '::' . $module => "PlackX::Framework::$module") unless $loaded{$module};
+    # Create it
+    if (!$loaded{$module} and $modules{$module}->{auto_create_subclass}) {
+      generate_subclass($caller . '::' . $module => "PlackX::Framework::$module");
     }
   }
 
@@ -46,9 +49,9 @@ sub import {
 }
 
 sub export_app_sub {
-  my $caller = shift;
+  my $destination_package = shift;
   no strict 'refs';
-  *{$caller . '::app'} = sub {
+  *{$destination_package . '::app'} = sub {
     my $class     = shift;
     my $app_class = $class . '::App';
     $app_class->to_app;
@@ -58,14 +61,12 @@ sub export_app_sub {
 sub load_subclass {
   my $class   = shift;
   my $module  = shift;
-  my $success = eval "require $class\::$module; 1;";
+  my $success = eval "require $class" . '::' . "$module; 1;";
   return $success;
 }
 
 sub generate_subclass {
   my ($new_class, $base_class) = @_;
-
-  # Create the package/class - must use string eval
   eval qq{
     package $new_class;
     use parent '$base_class';
@@ -88,8 +89,8 @@ A simple PlackX::Framework application could be all in one .psgi file:
 
     # app.psgi
     package MyProject {
-      use PlackX::Framework;  # use, NOT use 'parent'
-      use MyProject::Router;  # exports `request', 'request_base', and 'filter'
+      use PlackX::Framework; # use, NOT use 'parent'
+      use MyProject::Router; # exports `request', 'request_base', and 'filter'
       request '/' => sub {
          my ($request, $response) = @_;
          $response->body('Hello, ', $request->param('name'));
@@ -102,10 +103,14 @@ However, a larger application would be typically laid out with separate modules
 in separate files, for example in MyProject::Controller::* modules. Each should
 use MyProject::Router if the DSL-style routing is desired.
 
+This software is considered to be in an experimental, "alpha" stage. Use at 
+your own risk.
+
 
 =head1 DESCRIPTION
 
 PlackX::Framework consists of the following modules:
+
 PlackX::Framework
 PlackX::Framework::App
 PlackX::Framework::Request;
@@ -152,7 +157,7 @@ After converting a mod_perl2 web application to use Plack instead, where
 Plack::Request and Plack::Response replaced Apache2::Request and
 Apache2::Response, I realized I was basically using Plack as a low-level
 framework with some glue code and extra features. That framework has been
-extracted, refined, and expanded into this product.
+extracted, refactored, and expanded into this product.
 
 The end result is a simple, lightweight framework that is higher level
 than using the raw Plack building blocks, although it does not have as many
@@ -160,22 +165,24 @@ features as other frameworks. Here are some advantages:
 
  - A basic PlackX::Framework "Hello World" application loads 75% faster
    than a Dancer2 application and 70% faster than a Mojolicious::Lite app.
-   (Note that application execution time has not been benchmarked.)
+   (The author has not benchmarked request/response times.)
 
  - A basic PlackX::Framework "Hello World" application uses approximately
    one-third the memory of either Dancer2 or Mojolicious::Lite (~10MB compared
    to ~30MB for each of the other two).
 
  - PlackX::Framework has few non-core dependencies (it has more than 
-   Mojolicious, which has none, but much fewer than Dancer2, which
-   has a lot.)
+   Mojolicious, which has zero, but fewer than Dancer2, which has a lot.)
 
  - PlackX::Framework has some magic, but not too much. It can be easily
    overriden with subclassing. You can use the bundled router engine
    or supply your own. You can use Template Toolkit automagically or use
    a different template engine.
 
-If the above isn't enough justification, then this should be: TIMTOWTDI.
+If the above isn't enough justification for this module's mere existence,
+then this should be: TIMTOWTDI. The author makes no claims that this framework
+is better than any other framework except for the few trivial ones described
+above.
 
 
 =head2 Object Orientation
@@ -184,9 +191,12 @@ PlackX::Framework has an object-oriented design philosophy that uses both
 inheritance and composition to implement its features. Symbols exported are
 limited to avoid polluting your namespace, however, a lot of the "magic" is
 implemented with the import() method, so be careful about using empty
-parenthesis in your use statements. Also be careful about whether you should
-use a module or subclass it. Generally, modifying the behavior of the framework
-itself will involve subclassing, while using the framework will not.
+parenthesis in your use statements, as this will prevent the import() method
+from being called and may break some magic.
+
+Also be careful about whether you should use a module or subclass it.
+Generally, modifying the behavior of the framework itself will involve
+subclassing, while using the framework will not.
 
 
 =head2 Routes, Requests, and Request Filtering
@@ -223,7 +233,7 @@ get_template_system_object() method with your own code to create and/or
 retrieve your template system object.
 
 
-=head2 Databases and Object-Relational Mapping
+=head2 Model Layer
 
 This framework is databse/ORM agnostic, you are free to choose your own or use
 plain DBI/SQL.
@@ -239,8 +249,13 @@ to ::App->to_app.)
 =head1 Dependencies
 
 Plack
-URI::Fast
 Router::Boom
+
+
+=head1 Optional Dependencies
+
+URI::Fast
+Template
 
 
 =head1 SEE ALSO
@@ -253,12 +268,12 @@ Plack::Response
 
 =head1 AUTHOR
 
-Dondi Michael Stroma, E<lt>dstroma@localE<gt>
+Dondi Michael Stroma, E<lt>dstroma@gmail.com<gt>
 
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2016, 2021 by Dondi Stroma
+Copyright (C) 2016-2023 by Dondi Stroma
 
 
 =cut
